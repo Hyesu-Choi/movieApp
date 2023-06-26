fea6b4b5168429f8950866cf4b01f3d52efc5dd6
HyesuChoi <chlgotn0580@naver.com>
2023-06-26 13:01:39 +0900

    movie app
    code complete

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { fetchDataFromApi } from "../utils/api";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading("로딩중...");
+    setData(null);
+    setError(null);
+
+    fetchDataFromApi(url)
+      .then((res) => {
+        setLoading(false);
+        setData(res);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError("잘못된 접근입니다!");
+      });
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
